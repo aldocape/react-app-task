7d69b7f7ed70cea9bd863a4ec75e1fc05ec234c6
Aldo <aldocape@gmail.com>
2021-07-15 07:56:05 -0300

    Defino componentes react, creo archivo css y le agrego la clase DeColorRojo

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./App.css";
+
+function ArticleContainer(props) {
+  return <article>{props.children}</article>;
+}
+
+function Title(props) {
+  return <h1>{props.children}</h1>;
+}
+
+function Paragraph(props) {
+  return <p>{props.children}</p>;
+}
+
+function EnNegrita(props) {
+  return <strong>{props.children}</strong>;
+}
+
+function DeColorRojo(props) {
+  return <span className="DeColorRojo">{props.children}</span>;
+}
 
 function App() {
-  return <p>Hola!</p>;
+  return (
+    <ArticleContainer>
+      <Title>
+        Hola <i>Mundo</i>!
+      </Title>
+      <Paragraph>
+        Párrafo de mi artículo
+        <EnNegrita>
+          <DeColorRojo> importante</DeColorRojo>
+        </EnNegrita>!
+      </Paragraph>
+    </ArticleContainer>
+  );
 }
 
 ReactDOM.render(
